a2ef5789c5c96577a2644c0a95738ffb4d0d9140
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-06-11 18:22:17 +0200

    Trimmed the branch to only the modules/mrpt_math sub directory

--- modules/mrpt_expr/python/mrpt/expr/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-mrpt-expr: Runtime Compiled Expressions
-"""
-from . import _bindings as _b
-
-CRuntimeCompiledExpression = _b.CRuntimeCompiledExpression
-
-__all__ = ['CRuntimeCompiledExpression']
