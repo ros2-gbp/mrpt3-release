ca732efaaecdb48985c9cd38f7a5e79421f93110
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-07-03 09:07:45 +0200

    Trimmed the branch to only the modules/mrpt_viz sub directory

--- modules/mrpt_opengl/python/mrpt/opengl/__init__.py
+++
@@ -1,8 +0,0 @@
-import mrpt.viz  # noqa: F401
-
-from . import _bindings as _b
-
-CFBORender = _b.CFBORender
-CFBORenderParameters = _b.CFBORenderParameters
-
-__all__ = ['CFBORender', 'CFBORenderParameters']
